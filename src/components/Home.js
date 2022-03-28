0efbeceef3819ef1ce4f50cee120757edef09665
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-03-28 12:47:14 -0400

    commit tree 56359f733a4ec9e6428060d0f5b50c20b0f7be11

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const colorH1 = {
+  color: 'firebrick'
+}
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1 style={colorH1}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
